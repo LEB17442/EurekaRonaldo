b4a9608054e646b9459bdd15179e145614b5cce3
LUIS EDUARDO BARROCAS ALVES BITU <helderbitu13@gmail.com>
2025-06-08 22:39:23 -0300

    ultimo commit

--- discovery-service/src/test/java/service/discovery/DiscoveryApplicationTests.java
+++ discovery-service/src/test/java/service/discovery/DiscoveryApplicationTests.java
@@ -2,7 +2,7 @@ package service.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @SpringBootTest
 class DiscoveryApplicationTests {
